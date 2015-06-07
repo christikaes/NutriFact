2b8469243162e7833a180386a71981bfdedb026a
Ravi Singh <ravikrishnasingh@gmail.com>
2015-06-07 04:02:44 -0400

    more camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,38 +3,13 @@ angular.module('starter.controllers', [])
 .controller('NutriCtrl', function($scope) {
   $scope.cameraCapture = function() {
     navigator.camera.getPicture(function(result) {
-        console.log(result);
-        $scope.imageUri = result;
+        alert(result);
       }, function(err) {
-        console.log(err);
+        alert(err);
       }, { 
         quality: 50,
         destinationType: Camera.DestinationType.DATA_URL
       });
   };
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
+
