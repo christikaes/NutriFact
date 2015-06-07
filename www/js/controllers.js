0e3c3ff2ed38f1ce9448a42755fd23de0360b2fd
Ravi Singh <ravikrishnasingh@gmail.com>
2015-06-07 05:54:55 -0400

    all

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,7 @@ angular.module('starter.controllers', [])
         });
 
         var uploadUrl = "https://api.idolondemand.com/1/api/sync/ocrdocument/v1";
+
         var fd = new FormData();
         fd.append("apikey", "dd5e679c-3e9b-4ee6-ab4c-9db34501fb66");
         fd.append("mode", "document_scan");
@@ -16,19 +17,19 @@ angular.module('starter.controllers', [])
 
         $http.post(uploadUrl, fd, {
             transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
+            headers: {'Content-Type': "multipart/form-data"}
         })
         .success(function(result){
-          alert(JSON.stringify(result));
+          alert("success " + JSON.stringify(result));
         })
         .error(function(err){
-          alert(JSON.stringify(err));
+          alert("error " + JSON.stringify(err));
         });
+
       }, function(err) {
-        alert(err);
+        alert("camera error " + err);
       }, { 
-        destinationType: Camera.DestinationType.DATA_URI,
-        encodingType: Camera.EncodingType.JPEG
+        destinationType: Camera.DestinationType.FILE_URI
       });
   };
 });
