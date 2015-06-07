5ef00bf8ab06c673d0f77a20024d837e92b8ba0d
Ravi Singh <ravikrishnasingh@gmail.com>
2015-06-07 04:13:53 -0400

    more camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,10 @@ angular.module('starter.controllers', [])
 .controller('NutriCtrl', function($scope) {
   $scope.cameraCapture = function() {
     navigator.camera.getPicture(function(result) {
-        alert(result);
+        $scope.imageUri = result;
       }, function(err) {
         alert(err);
       }, { 
-        quality: 50,
         destinationType: Camera.DestinationType.DATA_URL
       });
   };
